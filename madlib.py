6aa3ab8f6e74ade83c1e327663a8ea7263111117
Vedika Gupta <vedika.gupta224@gmail.com>
2024-09-18 23:22:02 +0530

    first commit

---
+++ madlib.py
@@ -0,0 +1,12 @@
+adj1=input("adj1: ")
+adj2=input("adj2: ")
+adj3=input("adj3: ")
+adj4=input("adj4: ")
+adj5=input("adj5: ")
+adj6=input("adj6: ")
+adj7=input("adj7: ")
+
+madlib=f"I like Computer Science because {adj1}, it's very {adj2} and {adj3}. I'd like to be a {adj4} someday by developing\
+    my skills even further and doing {adj5} to the society! I love how I can build {adj6} and solve a lot of {adj7} problems."
+        
+print(madlib)
